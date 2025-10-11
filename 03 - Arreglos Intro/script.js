7235bc62b004016e9ebf3f743ea627492d4e8b6d
SirCrLs <102702780+SirCrLs@users.noreply.github.com>
2025-10-11 11:43:59 -0600

    agregado listas y busquedas a la pagina

--- 03 - Arreglos Intro/script.js
+++ 03 - Arreglos Intro/script.js
@@ -33,3 +33,45 @@ console.log(alumnos.indexOf("Regina"));
 console.log(alumnos.includes("Alejandro"));
 console.log(alumnos.find(nombre => nombre == "Regina"));
 console.log(alumnos.findIndex(nombre => nombre == "Carlos"));
+
+lista = document.getElementById("listaAlumnos");
+filtro = document.getElementById("filtroAlumno");
+btnConsulta = document.getElementById("consulta");
+
+function crearLista(){
+    for (let i = 0; i < alumnos.length; i++) {
+        nuevoAlumno = document.createElement('li');
+        nuevoAlumno.textContent = alumnos[i];
+        lista.appendChild(nuevoAlumno);
+    }
+}
+
+function encontrarAlumno(nombre){
+    liAlumnos = [];
+    for (let i = 0; i < alumnos.length; i++) { 
+        if(nombre == alumnos[i]){
+            liAlumnos.push(i);
+        }
+    }
+    console.log(liAlumnos);
+    return liAlumnos;
+}
+
+crearLista();
+
+btnConsulta.addEventListener("click",function(){
+    lista.textContent = "";
+    if(alumnos.includes(filtro.value)){
+        pos = encontrarAlumno(filtro.value)
+        for (let i = 0; i < pos.length; i++) {
+            nuevoAlumno = document.createElement('li');
+            nuevoAlumno.textContent = alumnos[pos[i]];
+            lista.appendChild(nuevoAlumno);
+        }
+    }else{
+        if(filtro.value == ""){crearLista();}
+        
+    }
+    
+})
+
